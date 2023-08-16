090c92799464ef0ece406e44a524d38d25d2f851
chandan kumar <106027583+chandan063@users.noreply.github.com>
2023-08-16 11:48:45 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,27 @@
 "use Strict";//treat all js code as newer version
 //alert(3+3)//we are using nodejs , not browser
 
+console.log(3
+    +3)
+console.log("hello");
+//Above code will run but we need to maintian the readability ;
+
+let name="hello"
+let age=89;
+let isLoggedIn=true;
+let temperature=null;
+
+//numbe => 2 to power 53
+// nigint
+// string=>""
+// boolean=>true/false
+// null=>standalone value
+// undefined=>
+// symbol=>unique
+
+//object
+
+console.log(typeof"hello");
+console.log(typeof(age));
+console.log(typeof undefined);//undefined is a type
+console.log(typeof null);//null is object

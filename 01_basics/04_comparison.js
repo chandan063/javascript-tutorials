a3217748cd91c913c73c25e1eddb2e0ed4bacfd1
chandan kumar <106027583+chandan063@users.noreply.github.com>
2023-08-18 16:38:28 +0000

    comparison data discussion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,27 @@
+// console.log( 2 > 3);
+// console.log( 3!=4);
+// console.log(2==2);
+
+console.log( "2" > 1); //true
+
+console.log( "03" > 1); //true
+
+console.log(null > 0)
+// false
+console.log(null == 0)
+// false
+console.log(null >=0);
+// trues
+
+console.log(undefined > 0)
+// false
+console.log(undefined == 0)
+// false
+console.log(undefined >=0);
+// false
+
+// 
+console.log("2" == 2)
+// true here it convert the string into number
+console.log("2" === 2)
+// false here its checks for data type and don't do conversion

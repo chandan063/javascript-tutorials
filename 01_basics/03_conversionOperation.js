0f0c834a7f284a009ea24d0ae855fdd69fd161d3
chandan kumar <106027583+chandan063@users.noreply.github.com>
2023-08-18 15:51:22 +0000

    conversonOperations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,53 @@
+let score="33";
+
+// console.log(typeof(score));
+
+let valueInNumber=Number(score);
+// console.log(typeof(valueInNumber));
+
+// when we change the given value
+// "33"=>33
+// "33asv"=>NAN
+// true=>1; false=>0;
+// null=>object; undefined=>undefined 
+
+let isLoggedIn="hello";
+// isLoggedIn=1=>true
+// isLoggedIn=0=>false
+// isLogged=" "=>false
+// isLogged="chand"=>true;
+let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+let someNumber=33;
+let stringNumber=String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// &&&&&&&&&&&&&& Operations &&&&&&&&&&&&
+
+let value=99;
+let negValue=-value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1="ram"
+let str2=" kumar"
+let str3=str1+str2;
+// console.log(str3);
+
+console.log(1+"2");//12
+console.log("1"+2);//12
+console.log("1"+2+2);//122
+console.log(1+2+"3");//33
+
+console.log(true);//o/p=>true
+console.log(+true);//o/p=>1
+console.log(+" ");//o/p=>1
